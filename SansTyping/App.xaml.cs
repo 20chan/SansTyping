3494adb79b0d22c7f7e020b0e2fe9b5cda4397cb
20chan <2@0chan.dev>
2020-08-19 00:30:40 +0900

    init wpf project

---
+++ SansTyping/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SansTyping {
+    /// <summary>
+    /// App.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class App : Application {
+    }
+}
